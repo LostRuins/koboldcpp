b4339b4ad7e8a6b5190bbe03404160db5c6f39bc
Rose22 <rosemelody22@gmail.com>
2025-11-22 02:03:07 +0100

    experimental further compression of tools array, needs testing

--- koboldcpp.py
+++ koboldcpp.py
@@ -2666,14 +2666,15 @@ def compress_tools_array(tools_array):
     tools_array_filtered = []
     for tool_dict in tools_array:
         tool_data = tool_dict['function']
-        tool_props = {}
-        for prop_name, prop_data in tool_data['parameters']['properties'].items():
-            tool_props[prop_name] = prop_data['type']
-        tools_array_filtered.append({
-            "name": tool_data['name'],
-            "description": tool_data['description'],
-            "properties": tool_props
-        })
+        tools_array_filtered.append(tool_data['name'])
+        # tool_props = {}
+        # for prop_name, prop_data in tool_data['parameters']['properties'].items():
+        #     tool_props[prop_name] = prop_data['type']
+        # tools_array_filtered.append({
+        #     "name": tool_data['name'],
+        #     "description": tool_data['description'],
+        #     "properties": tool_props
+        # })
 
     return tools_array_filtered
 
