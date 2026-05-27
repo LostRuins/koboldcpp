27f9b23a08bbbafa6bbe7f2bc5226658912050d6
Wagner Bruna <wbruna@yahoo.com>
2026-05-27 09:43:20 -0300

    sd: split gpt-oss vocab and merge files

--- otherarch/sdcpp/sdtype_adapter.cpp
+++ otherarch/sdcpp/sdtype_adapter.cpp
@@ -279,18 +279,25 @@ std::string load_umt5_tokenizer_json()
     umt5str = read_str_from_disk(filepath);
     return umt5str;
 }
-// TODO replace with embd files
-#include "tokenizers/vocab/gpt_oss_merges.hpp"
-#include "tokenizers/vocab/gpt_oss_vocab.hpp"
 std::string load_gpt_oss_merges()
 {
-    std::string merges_utf8_str(reinterpret_cast<const char*>(gpt_oss_merges_utf8_c_str), sizeof(gpt_oss_merges_utf8_c_str));
-    return merges_utf8_str;
+    static std::string mergesstr;  // cached string
+    if (!mergesstr.empty()) {
+        return mergesstr;  // already loaded
+    }
+    std::string filepath = executable_path + "embd_res/gpt_oss_merges_utf8_c_str.embd";
+    mergesstr = read_str_from_disk(filepath);
+    return mergesstr;
 }
 std::string load_gpt_oss_vocab_json()
 {
-    std::string json_str(reinterpret_cast<const char*>(gpt_oss_vocab_json_utf8_c_str), sizeof(gpt_oss_vocab_json_utf8_c_str));
-    return json_str;
+    static std::string vocabstr;  // cached string
+    if (!vocabstr.empty()) {
+        return vocabstr;  // already loaded
+    }
+    std::string filepath = executable_path + "embd_res/gpt_oss_vocab_json.embd";
+    vocabstr = read_str_from_disk(filepath);
+    return vocabstr;
 }
 
 static std::string get_device_override(int value, const char * module = nullptr)
