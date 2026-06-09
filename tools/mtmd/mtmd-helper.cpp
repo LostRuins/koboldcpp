141f74af10484fc303a86f1adfe562a435443c17
Wagner Bruna <wbruna@yahoo.com>
2026-06-09 13:13:42 -0300

    mtmd: fix non-Windows build

--- tools/mtmd/mtmd-helper.cpp
+++ tools/mtmd/mtmd-helper.cpp
@@ -1,18 +1,10 @@
-// fix problem with std::min and std::max
-#if defined(_WIN32)
-#define WIN32_LEAN_AND_MEAN
-#ifndef NOMINMAX
-#   define NOMINMAX
-#endif
-#include <windows.h>
-#endif
-
 #include "mtmd.h"
 #include "mtmd-helper.h"
 #include "llama.h"
 
 #include <algorithm>
 #include <cinttypes>
+#include <cstring>
 #include <vector>
 
 //#define MTMD_AUDIO_DEBUG
