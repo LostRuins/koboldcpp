109e672fe11ea9c7ea3d7f02001470f0b0f3fd1e
Wagner Bruna <wbruna@yahoo.com>
2026-09-12 12:37:43 -0300

    bump GGML_MAX_NAME to 160
    https://github.com/leejet/stable-diffusion.cpp/pull/1950

--- ggml/include/ggml.h
+++ ggml/include/ggml.h
@@ -226,7 +226,7 @@
 #define GGML_MAX_OP_PARAMS      64
 
 #ifndef GGML_MAX_NAME
-#   define GGML_MAX_NAME        128
+#   define GGML_MAX_NAME        160
 #endif
 
 #define GGML_DEFAULT_N_THREADS  4
